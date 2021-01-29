53cea657094b4398fb6353e7a65cb7dd8a10acd0
m4dSt4cks <johnsonjspx@gmail.com>
2021-01-28 21:03:06 -0500

    Create challenge.py

---
+++ challenge.py
@@ -0,0 +1,7 @@
+from hacksport.docker import DockerChallenge, HTTP
+
+
+class Problem(DockerChallenge):
+    def setup(self):
+        self.ports = { 1823: HTTP("Description of port, ex. website", path="/optional", link_test="also optional, ex. my website") }
+        self.initialize_docker({})
