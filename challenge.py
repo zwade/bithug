ccce5fedfe1f526115adb231db6653dfc6b757f7
m4dSt4cks <johnsonjspx@gmail.com>
2021-01-28 21:04:13 -0500

    Update challenge.py

--- challenge.py
+++ challenge.py
@@ -5,3 +5,6 @@ class Problem(DockerChallenge):
     def setup(self):
         self.ports = { 1823: HTTP("Description of port, ex. website", path="/optional", link_test="also optional, ex. my website") }
         self.initialize_docker({})
+
+    def generate_flag(self, random):
+        return "flag goes here"
